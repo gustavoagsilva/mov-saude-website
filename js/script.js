e8f831ba8215cd77a46b316aa344790b2c8606e1
Gustavo Augusto <gustavoags2000@gmail.com>
2026-05-04 14:25:43 -0300

    feat: add CSS

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-const elements = document.querySelectorAll('.reveal');
+const elements = document.querySelectorAll(".reveal");
 
 function revealOnScroll() {
-    const windowHeight = window.innerHeight;
+  const windowHeight = window.innerHeight;
 
-    elements.forEach(el => {
-        const elementTop = el.getBoundingClientRect().top;
-        const elementBottom = el.getBoundingClientRect().bottom;
+  elements.forEach((el) => {
+    const elementTop = el.getBoundingClientRect().top;
+    const elementBottom = el.getBoundingClientRect().bottom;
 
-        if (elementTop < windowHeight - 100 && elementBottom > 0) {
-            el.classList.add('active');
-        } else {
-            el.classList.remove('active');
-        }
-    });
+    if (elementTop < windowHeight - 100 && elementBottom > 0) {
+      el.classList.add("active");
+    } else {
+      el.classList.remove("active");
+    }
+  });
 }
-addEventListener('scroll', revealOnScroll);
+addEventListener("scroll", revealOnScroll);
 
 elements.forEach((el, index) => {
-    const elementTop = el.getBoundingClientRect().top;
+  const elementTop = el.getBoundingClientRect().top;
 
-    if (elementTop < windowHeight - 100) {
-        el.style.transitionDelay = `${index * 0.1}s`;
-        el.classList.add('active');
-    }
+  if (elementTop < windowHeight - 100) {
+    el.style.transitionDelay = `${index * 0.1}s`;
+    el.classList.add("active");
+  }
 });
